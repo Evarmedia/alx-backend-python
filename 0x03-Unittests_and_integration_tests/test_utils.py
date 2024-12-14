182ae430e4f6a90c6934976f1f98238b08dcd493
Evarmedia <mishakmanuel@gmail.com>
2024-12-14 18:36:24 +0100

    chore: added task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,24 @@ class TestAccessNestedMap(unittest.TestCase):
         :param expected: Expected return value
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """
+        Parameterized test for access_nested_map function exceptions.
+        
+        :param nested_map: Input nested map
+        :param path: Path to access in the nested map
+        :param expected_key: Expected key in the exception message
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        
+        # Check that the exception message contains the expected key
+        self.assertEqual(str(context.exception), repr(expected_key))
 
 if __name__ == '__main__':
     unittest.main()
