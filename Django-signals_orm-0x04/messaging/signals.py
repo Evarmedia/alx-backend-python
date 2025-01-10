22743b0a40d8152ebb0b8983a24b6c6bb36fd885
Evarmedia <mishakmanuel@gmail.com>
2025-01-10 15:20:34 +0100

    fix: fixed signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,17 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from .models import Message, MessageHistory
+from .models import Message, MessageHistory, Notification
+
+@receiver(post_save, sender=Message)
+def create_notification(sender, instance, created, **kwargs):
+    """
+    Automatically creates a notification for the receiver when a new message is sent.
+    """
+    if created:  # Ensure this runs only when a new message is created
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
